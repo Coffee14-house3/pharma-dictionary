72653960015f6a4eed89e27991082f1866a7298a
coffee14-house3 <prathikaa.d.gupta@gmail.com>
2026-05-31 18:42:37 +0530

    Setup GitHub Pages auto-deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pharma-dictionary/',
   server: {
     watch: {
       ignored: ['**/node_modules/**', '**/.git/**', '**/AppData/**', '**/dist/**'],
